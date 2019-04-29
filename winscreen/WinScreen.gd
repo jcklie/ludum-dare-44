3fd766ea490ec6f47ae20918c0d95286f96ea1e3
Jan-Christoph Klie <git@mrklie.com>
2019-04-29 18:54:26 +0200

    Win screen

--- winscreen/WinScreen.gd
+++ winscreen/WinScreen.gd
@@ -1,9 +1,17 @@
 extends Control
 
+var win_textures = {
+	Global.Currency.Euro: preload("res://winscreen/eur_profile.png"),
+	Global.Currency.Dollar: preload("res://winscreen/usd_profile.png"),
+	Global.Currency.Yen: preload("res://winscreen/yen_profile.png"),
+	Global.Currency.Pound: preload("res://winscreen/gbp_profile.png")
+}
+
 func _ready():
 	var winning_text = "Player %s wins" % GameManager.winning_player
 	$WinLabel.text = winning_text
+	$WinPic.texture = win_textures[GameManager.winning_currency]
 	
 func _process(delta):
-	if Input.action_release("key_continue"):
+	if Input.is_action_pressed("key_continue"):
 		GameManager.start_random_level()
