f152771a8390f519f3e0d836703a596ed6b41730
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2019-04-29 20:30:07 +0200

    Add map

--- levels/GameManager.gd
+++ levels/GameManager.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 var levels = [
 	#preload("res://levels/Arena.tscn")
-	preload("res://levels/EmptyArena.tscn")
+	preload("res://levels/EmptyArena.tscn"),
+	preload("res://levels/BlockFortKnox.tscn")
 ]
 
 var level_class = "res://levels/Level.tscn"
